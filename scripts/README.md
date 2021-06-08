bfc23ead1cfbfa34b15c3fdf8c108cb016e8cba5
Ed Mckain <mckain.default@gmail.com>
2021-06-08 15:06:40 +0200

    adds a bash script to export env vars for AWS MFA auth

---
+++ scripts/README.md
@@ -0,0 +1,16 @@
+
+
+### Dependencies
+
+#### JQ
+
+If you are on MacOS:
+
+```
+brew install jq
+```
+
+### Run
+
+`source aws_bastion_mfa_export.sh -p <profile-name>`
+
---
+++ scripts/aws_bastion_mfa_export.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+while getopts p: flag
+do
+    case "${flag}" in
+        p) profile=${OPTARG};;
+    esac
+done
+
+echo "|_ Using profile flag: $profile";
+
+# Make sure we have a temporary token
+# This will also request your MFA token if needed
+echo "|_ Requesting identity with profile $profile"
+aws sts get-caller-identity --profile $profile
+
+cachedir=~/.aws/cli/cache
+creds_json=$(cat $cachedir/*json)
+
+AWS_ACCESS_KEY_ID=$(echo "$creds_json" | jq -r .Credentials.AccessKeyId)
+AWS_SECRET_ACCESS_KEY=$(echo "$creds_json" | jq -r .Credentials.SecretAccessKey)
+AWS_SESSION_TOKEN=$(echo "$creds_json"  | jq -r .Credentials.SessionToken)
+
+echo exporting AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN
+
+exp=$(echo "$creds_json" | jq -r .Credentials.Expiration)
+echo "|_ Current token expires at: $exp"
