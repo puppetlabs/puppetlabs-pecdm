a721d9a8ca2614ae58d2dd87431d66db018a04d5
Edwin Maldonado <mckain.default@gmail.com>
2021-06-10 12:00:44 +0200

    adds basic MFA instructions on scripts/README (#35)
    * updates MFA section on the main README
    
    * adds a bash script to export env vars for AWS MFA auth
    
    * removes the old examples/export-profile.py script
    
    * adds some validation & error messages for arguments
    
    * updates file permissions to bash script to be ready after clone

---
+++ scripts/aws_bastion_mfa_export.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+    printf 'no argument flags provided\n' >&2
+    exit 1
+fi
+
+while getopts ":p:" flag; do
+    case "$flag" in
+        p ) profile=${OPTARG};;
+        \? ) echo "Invalid option -$OPTARG" 1>&2; exit 1;;
+        : ) echo "Invalid option -$OPTARG requires argument" 1>&2; exit 1;;
+    esac
+done
+
+if [ -z ${profile} ]; then
+    echo "profile is required" >&2; exit 1
+fi
+
+echo "|_ Using profile flag: $profile";
+
+# Make sure we have a temporary token
+# This will also request your MFA token if needed
+echo "|_ Requesting identity with profile $profile"
+aws sts get-caller-identity --profile "$profile"
+
+cachedir=~/.aws/cli/cache
+creds_json=$(cat "$cachedir"/*json)
+
+AWS_ACCESS_KEY_ID=$(echo "$creds_json" | jq -r .Credentials.AccessKeyId)
+AWS_SECRET_ACCESS_KEY=$(echo "$creds_json" | jq -r .Credentials.SecretAccessKey)
+AWS_SESSION_TOKEN=$(echo "$creds_json"  | jq -r .Credentials.SessionToken)
+
+echo exporting AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN
+
+exp=$(echo "$creds_json" | jq -r .Credentials.Expiration)
+echo "|_ Current token expires at: $exp"
