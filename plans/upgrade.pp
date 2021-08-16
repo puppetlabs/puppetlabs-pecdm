81adc5e1cd1e4bc185f0083fe9cc51c91ed38609
Ed Mckain <mckain.default@gmail.com>
2021-08-16 15:47:25 +0200

    replaces the use of master for server & primary_host
    WIP
    
    adapts autope::upgrade to deal with new param names

--- plans/upgrade.pp
+++ plans/upgrade.pp
@@ -1,9 +1,20 @@
 plan autope::upgrade(
-  TargetSpec                           $targets          = get_targets('peadm_nodes'),
-  String                               $version          = '2019.3.0',
-  String                               $ssh_user,
-  Enum['xlarge', 'large', 'standard']  $architecture     = 'xlarge',
-  Enum['google', 'aws', 'azure']       $provider         = 'google'
+  TargetSpec                           $targets             = get_targets('peadm_nodes'),
+  String                               $version             = '2019.3.0',
+  # String[1]                            $console_password    = 'puppetlabs',
+  Integer                              $compiler_count      = 1,
+  Optional[String[1]]                  $ssh_pub_key_file    = undef,
+  Optional[Integer]                    $node_count          = undef,
+  Optional[String[1]]                  $instance_image      = undef,
+  Optional[String[1]]                  $stack               = undef,
+  Array                                $firewall_allow      = [],
+  Hash                                 $extra_peadm_params  = {},
+  Boolean                              $replica             = false,
+  Enum['xlarge', 'large', 'standard']  $architecture        = 'standard',
+  Enum['google', 'aws', 'azure']       $provider            = 'aws',
+  String[1]                            $project             = $provider ? { 'aws' => 'ape', default => undef },
+  String[1]                            $ssh_user            = $provider ? { 'aws' => 'centos', default => undef },
+  String[1]                            $cloud_region        = $provider ? { 'azure' => 'westus2', 'aws' => 'us-west-2', default => 'us-west1' }, # lint:ignore:140chars
 ) {
 
   Target.new('name' => 'localhost', 'config' => { 'transport' => 'local'})
@@ -11,8 +22,38 @@ plan autope::upgrade(
   $tf_dir = ".terraform/${provider}_pe_arch"
 
   if $provider == 'aws' {
-    waring('AWS provider is currently expiremental and may change in a future release')
+    warning('AWS provider is currently expiremental and may change in a future release')
   }
+
+  # WIP
+  $tfvars = inline_epp(@(TFVARS))
+    project        = "<%= $project %>"
+    user           = "<%= $ssh_user %>"
+    <% unless $ssh_pub_key_file == undef { -%>
+    ssh_key        = "<%= $ssh_pub_key_file %>"
+    <% } -%>
+    region         = "<%= $cloud_region %>"
+    compiler_count = <%= $compiler_count %>
+    <% unless $node_count == undef { -%>
+    node_count     = "<%= $node_count %>"
+    <% } -%>
+    <% unless $instance_image == undef { -%>
+    instance_image = "<%= $instance_image %>"
+    <% } -%>
+    <% unless stack == undef { -%>
+    stack_name     = "<%= $stack %>"
+    <% } -%>
+    firewall_allow = <%= String($firewall_allow).regsubst('\'', '"', 'G') %>
+    architecture   = "<%= $architecture %>"
+    replica        = <%= $replica %>
+    | TFVARS
+
+  # TODO: make this print only when user specifies --verbose
+  out::verbose(".tfvars file content:\n\n${tfvars}\n")
+
+  $terraform_output = run_task('terraform::output', 'localhost', dir => $tf_dir)
+  $applied = $terraform_output.first
+  #
 
   $target_config = {
     'config' => {
@@ -25,7 +66,7 @@ plan autope::upgrade(
     }
   }
 
-  $inventory = ['master', 'psql', 'compiler' ].reduce({}) |Hash $memo, String $i| {
+  $inventory = ['server', 'psql', 'compiler' ].reduce({}) |Hash $memo, String $i| {
     $memo + { $i => resolve_references({
         '_plugin'        => 'terraform',
         'dir'            => $tf_dir,
@@ -56,41 +97,52 @@ plan autope::upgrade(
     Target.new($target.merge($target_config)).add_to_group('peadm_nodes')
   }}
 
+  # Debugging
+  # debug::break()
+
   case $architecture {
     'xlarge': {
       $params = {
-        'master_host'                    => $inventory['master'][0]['name'],
-        'master_replica_host'            => $inventory['master'][1]['name'],
-        'puppetdb_database_host'         => $inventory['psql'][0]['name'],
-        'puppetdb_database_replica_host' => $inventory['psql'][1]['name'],
+        'primary_host'                   => $inventory['server'][0]['name'],
+        'replica_host'                   => $inventory['server'][1]['name'],
+        'primary_postgresql_host'        => $inventory['psql'][0]['name'],
+        'replica_postgresql_host'        => $inventory['psql'][1]['name'],
         'compiler_hosts'                 => $inventory['compiler'].map |$c| { $c['name'] },
-        'console_password'               => $console_password,
-        'dns_alt_names'                  => [ 'puppet', $apply['pool']['value'] ],
-        'compiler_pool_address'          => $apply['pool']['value'],
-        'version'                        => $version
+        # 'console_password'               => $console_password,
+        # 'dns_alt_names'                  => [ 'puppet', $applied['pool']['value'] ],
+        'compiler_pool_address'          => $applied['pool']['value'],
+        'version'                        => $version,
+        'download_mode'                  => 'direct',
       }
     }
     'large': {
       $params = {
-        'master_host'                    => $inventory['master'][0]['name'],
+        'primary_host'                    => $inventory['server'][0]['name'],
         'compiler_hosts'                 => $inventory['compiler'].map |$c| { $c['name'] },
-        'console_password'               => $console_password,
-        'dns_alt_names'                  => [ 'puppet', $apply['pool']['value'] ],
-        'compiler_pool_address'          => $apply['pool']['value'],
-        'version'                        => $version
+        # 'console_password'               => $console_password,
+        # 'dns_alt_names'                  => [ 'puppet', $applied['pool']['value'] ],
+        'compiler_pool_address'          => $applied['pool']['value'],
+        'version'                        => $version,
+        'download_mode'                  => 'direct',
       }
     }
     'standard': {
       $params = {
-        'master_host'                    => $inventory['master'][0]['name'],
-        'console_password'               => $console_password,
-        'dns_alt_names'                  => [ 'puppet', $apply['pool']['value'] ],
-        'compiler_pool_address'          => $apply['pool']['value'],
-        'version'                        => $version
+        'primary_host'                    => $inventory['server'][0]['name'],
+        # 'console_password'               => $console_password,
+        # 'dns_alt_names'                  => [ 'puppet', $applied['pool']['value'] ],
+        'compiler_pool_address'          => $applied['pool']['value'],
+        'version'                        => $version,
+        'download_mode'                  => 'direct',
       }
     }
     default: { fail('Something went horribly wrong or only xlarge is supported in this configuration') }
   }
+
+  # Debugging
+  # debug::break()
+  # TODO: make this print only when user specifies --verbose
+  out::verbose("params var content:\n\n${params}\n")
 
   # Once all the infrastructure data has been collected, peadm takes over
   run_plan('peadm::upgrade', $params)
